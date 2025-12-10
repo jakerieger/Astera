734e8beda5c967f3c1c55043ac0c1e2a5be9a5a4
jakerieger <jakerieger@users.noreply.github.com>
2025-12-10 23:05:21 +0000

    deploy: 268f9357f47a47d84575678cb97c42699a0932e8

--- navtreedata.js
+++ navtreedata.js
@@ -24,7 +24,7 @@
 */
 var NAVTREE =
 [
-  [ "Nth Engine", "index.html", [
+  [ "Astera", "index.html", [
     [ "Namespaces", "namespaces.html", [
       [ "Namespace List", "namespaces.html", "namespaces_dup" ],
       [ "Namespace Members", "namespacemembers.html", [
